7a69456b193b574af091aa4be46f34dee7ba4efa
Osmar Alejandro Liera Gomez <osmar2001lg@gmail.com>
2026-04-13 14:20:27 -0700

    Pagares

--- resources/views/pdfs/pagares.blade.php
+++ resources/views/pdfs/pagares.blade.php
@@ -19,24 +19,42 @@
             padding: 0;
         }
 
+        /*
+         * Hoja legal portrait: 35.56cm de alto
+         * Márgenes @page: 0.4cm top + 0.4cm bottom = 0.8cm
+         * Disponible: 34.76cm
+         * 4 pagarés + 3 separaciones de 0.1cm = 0.3cm
+         * Cada wrapper: (34.76 - 0.3) / 4 = 8.615cm → usamos 8.6cm
+         */
         .pagare-wrapper {
-            padding: 0;
-            margin-bottom: 4px;
+            height: 8.6cm;
+            overflow: hidden;
+            margin-bottom: 0.1cm;
             page-break-inside: avoid;
+            box-sizing: border-box;
+        }
+
+        .pagare-wrapper:last-child {
+            margin-bottom: 0;
         }
 
         .pagare-container {
             border: 4px double #0d3b66;
             border-radius: 8px;
             padding: 4px;
+            height: 100%;
             position: relative;
             background-color: #fff;
+            box-sizing: border-box;
         }
 
         .inner-content {
             border: 1px solid #0d3b66;
             border-radius: 6px;
             padding: 5px;
+            height: calc(100% - 10px);
+            box-sizing: border-box;
+            overflow: hidden;
         }
 
         table {
@@ -50,7 +68,7 @@
             vertical-align: middle;
             padding: 0;
         }
-        
+
         /* Rounded corners for top header cells */
         .table-blue tr:first-child td:first-child { border-top-left-radius: 6px; }
         .table-blue tr:first-child td:last-child { border-top-right-radius: 6px; }
@@ -169,118 +187,122 @@
             margin-top: 1px;
         }
 
-        .page-break {
+        .page-group {
             page-break-after: always;
+        }
+
+        .page-group:last-child {
+            page-break-after: auto;
         }
     </style>
 </head>
 
 <body>
-    @foreach ($pagares as $index => $pagare)
-        <div class="pagare-wrapper">
-            <div class="pagare-container">
-                <div class="inner-content">
-                    <table class="table-blue">
-                        <tr>
-                            <td style="width: 15%;">
-                                <span class="label-header">PAGARÉ No.</span>
-                                <span class="value-header">{{ $pagare['numero_display'] }}</span>
-                            </td>
+    @foreach ($pagares->chunk(4) as $grupo)
+        <div class="page-group">
+            @foreach ($grupo as $pagare)
+                <div class="pagare-wrapper">
+                    <div class="pagare-container">
+                        <div class="inner-content">
+                            <table class="table-blue">
+                                <tr>
+                                    <td style="width: 15%;">
+                                        <span class="label-header">PAGARÉ No.</span>
+                                        <span class="value-header">{{ $pagare['numero_display'] }}</span>
+                                    </td>
 
-                            <td style="width: 25%;">
-                                <span class="label-header">FECHA DE EXPEDICIÓN</span>
-                                <table class="date-sub-table">
-                                    <tr>
-                                        <td><span class="date-label">DIA</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('d') }}</span></td>
-                                        <td><span class="date-label">MES</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('m') }}</span></td>
-                                        <td><span class="date-label">AÑO</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('Y') }}</span></td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td style="width: 25%;">
-                                <span class="label-header">FECHA DE VENCIMIENTO</span>
-                                <table class="date-sub-table">
-                                    <tr>
-                                        <td><span class="date-label">DIA</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('d') }}</span></td>
-                                        <td><span class="date-label">MES</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('m') }}</span></td>
-                                        <td><span class="date-label">AÑO</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('Y') }}</span></td>
-                                    </tr>
-                                </table>
-                            </td>
-                            <td style="width: 15%;">
-                                <span class="label-header">CONTRATO No.</span>
-                                <span class="value-header">{{ $pagare['folio'] }}</span>
-                            </td>
-                            <td style="width: 20%; border-top-right-radius: 6px;">
-                                <span class="label-header">BUENO POR:</span>
-                                <table style="width: 100%;">
-                                    <tr>
-                                        <td style="border: none; width: 25%; font-size: 10pt; font-weight: bold; color: #0d3b66;">$</td>
-                                        <td style="border: none; text-align: right; padding-right: 4px;">
-                                            <span style="font-size: 9pt; font-weight: bold;">{{ number_format($pagare['monto'], 2) }}</span>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
+                                    <td style="width: 25%;">
+                                        <span class="label-header">FECHA DE EXPEDICIÓN</span>
+                                        <table class="date-sub-table">
+                                            <tr>
+                                                <td><span class="date-label">DIA</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('d') }}</span></td>
+                                                <td><span class="date-label">MES</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('m') }}</span></td>
+                                                <td><span class="date-label">AÑO</span><span class="date-value">{{ $pagare['fecha_expedicion']->format('Y') }}</span></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td style="width: 25%;">
+                                        <span class="label-header">FECHA DE VENCIMIENTO</span>
+                                        <table class="date-sub-table">
+                                            <tr>
+                                                <td><span class="date-label">DIA</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('d') }}</span></td>
+                                                <td><span class="date-label">MES</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('m') }}</span></td>
+                                                <td><span class="date-label">AÑO</span><span class="date-value">{{ $pagare['fecha_vencimiento']->format('Y') }}</span></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td style="width: 15%;">
+                                        <span class="label-header">CONTRATO No.</span>
+                                        <span class="value-header">{{ $pagare['folio'] }}</span>
+                                    </td>
+                                    <td style="width: 20%; border-top-right-radius: 6px;">
+                                        <span class="label-header">BUENO POR:</span>
+                                        <table style="width: 100%;">
+                                            <tr>
+                                                <td style="border: none; width: 25%; font-size: 10pt; font-weight: bold; color: #0d3b66;">$</td>
+                                                <td style="border: none; text-align: right; padding-right: 4px;">
+                                                    <span style="font-size: 9pt; font-weight: bold;">{{ number_format($pagare['monto'], 2) }}</span>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
 
-                    <div class="text-main">
-                        LA PAZ, B.C.S., EL DIA <b>{{ $pagare['fecha_expedicion']->translatedFormat('d \d\e F \d\e Y') }}</b><br>
-                        DEBO(EMOS) Y PAGARE(MOS) INCONDICIONALMENTE POR ESTE PAGARE A LA ORDEN DE <b>{{ strtoupper($acreedor) }}</b>
-                        EN ESTA CIUDAD, EL DIA <b>{{ $pagare['fecha_vencimiento']->translatedFormat('d \d\e F \d\e Y') }}</b>
-                        LA CANTIDAD DE:
+                            <div class="text-main">
+                                LA PAZ, B.C.S., EL DIA <b>{{ $pagare['fecha_expedicion']->translatedFormat('d \d\e F \d\e Y') }}</b><br>
+                                DEBO(EMOS) Y PAGARE(MOS) INCONDICIONALMENTE POR ESTE PAGARE A LA ORDEN DE <b>{{ strtoupper($acreedor) }}</b>
+                                EN ESTA CIUDAD, EL DIA <b>{{ $pagare['fecha_vencimiento']->translatedFormat('d \d\e F \d\e Y') }}</b>
+                                LA CANTIDAD DE:
+                            </div>
+
+                            <div class="monto-letras-centered">
+                                {{ $pagare['monto_letras'] }}
+                            </div>
+
+                            <div class="serie-text">
+                                VALOR RECIBIDO A MI(NUESTRA) ENTERA SATISFACCIÓN. ESTE PAGARE FORMA PARTE DE UNA SERIE NUMERADA DEL 1 AL {{ $pagare['total_letras'] }}
+                                Y TODOS ESTÁN SUJETOS A LA CONDICIÓN DE QUE, AL NO PAGARSE CUALQUIERA DE ELLOS A SU VENCIMIENTO SE CONSIDERARÁN VENCIDOS ANTICIPADAMENTE
+                                TODOS LOS QUE LE SIGAN EN NÚMERO, ADEMÁS DE LOS YA VENCIDOS, DESDE LA FECHA DE VENCIMIENTO DE ESTE DOCUMENTO HASTA EL DÍA DE SU LIQUIDACIÓN,
+                                CAUSARÁ INTERESES MORATORIOS AL TIPO DEL <b>10%</b> MENSUAL, PAGADERO EN ESTA CIUDAD JUNTAMENTE CON EL PRINCIPAL.
+                            </div>
+
+                            <table class="signatures-table">
+                                <tr>
+                                    <td class="signature-cell" style="width: 48%;">
+                                        <div class="signature-header">NOMBRE Y DIRECCIÓN DEL DEUDOR</div>
+                                        <div class="signature-body">
+                                            {{ strtoupper($venta->comprador->full_name) }}<br>
+                                            {{ strtoupper($venta->comprador->calle) }} {{ $venta->comprador->numero_exterior }}, COL. {{ strtoupper($venta->comprador->colonia) }}<br>
+                                            {{ strtoupper($venta->comprador->localidad_domicilio ?? 'LA PAZ, B.C.S.') }}, C.P. {{ $venta->comprador->codigo_postal }}<br>
+                                            TEL: {{ $venta->comprador->phones->first()->number ?? 'N/A' }}
+                                        </div>
+                                        <div class="firma-box" style="left: 12px;">
+                                            <div class="firma-line"></div>
+                                            <div class="firma-label">FIRMA</div>
+                                        </div>
+                                    </td>
+                                    <td style="width: 4%;"></td>
+                                    <td class="signature-cell" style="width: 48%;">
+                                        <div class="signature-header">NOMBRE Y DIRECCIÓN DEL AVAL</div>
+                                        <div class="signature-body">
+                                            {{ strtoupper($venta->aval->full_name ?? 'N/A') }}<br>
+                                            {{ strtoupper($venta->aval->calle ?? '') }} {{ $venta->aval->numero_exterior ?? '' }}, COL. {{ strtoupper($venta->aval->colonia ?? '') }}<br>
+                                            {{ strtoupper($venta->aval->localidad_domicilio ?? 'LA PAZ, B.C.S.') }}, C.P. {{ $venta->aval->codigo_postal ?? '' }}<br>
+                                            TEL: {{ $venta->aval->phones->first()->number ?? 'N/A' }}
+                                        </div>
+                                        <div class="firma-box" style="right: 12px;">
+                                            <div class="firma-line"></div>
+                                            <div class="firma-label">FIRMA</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
-
-                    <div class="monto-letras-centered">
-                        {{ $pagare['monto_letras'] }}
-                    </div>
-
-                    <div class="serie-text">
-                        VALOR RECIBIDO A MI(NUESTRA) ENTERA SATISFACCIÓN. ESTE PAGARE FORMA PARTE DE UNA SERIE NUMERADA DEL 1 AL {{ $pagare['total_letras'] }}
-                        Y TODOS ESTÁN SUJETOS A LA CONDICIÓN DE QUE, AL NO PAGARSE CUALQUIERA DE ELLOS A SU VENCIMIENTO SE CONSIDERARÁN VENCIDOS ANTICIPADAMENTE
-                        TODOS LOS QUE LE SIGAN EN NÚMERO, ADEMÁS DE LOS YA VENCIDOS, DESDE LA FECHA DE VENCIMIENTO DE ESTE DOCUMENTO HASTA EL DÍA DE SU LIQUIDACIÓN,
-                        CAUSARÁ INTERESES MORATORIOS AL TIPO DEL <b>10%</b> MENSUAL, PAGADERO EN ESTA CIUDAD JUNTAMENTE CON EL PRINCIPAL.
-                    </div>
-
-                    <table class="signatures-table">
-                        <tr>
-                            <td class="signature-cell" style="width: 48%;">
-                                <div class="signature-header">NOMBRE Y DIRECCIÓN DEL DEUDOR</div>
-                                <div class="signature-body">
-                                    {{ strtoupper($venta->comprador->full_name) }}<br>
-                                    {{ strtoupper($venta->comprador->calle) }} {{ $venta->comprador->numero_exterior }}, COL. {{ strtoupper($venta->comprador->colonia) }}<br>
-                                    {{ strtoupper($venta->comprador->localidad_domicilio ?? 'LA PAZ, B.C.S.') }}, C.P. {{ $venta->comprador->codigo_postal }}<br>
-                                    TEL: {{ $venta->comprador->phones->first()->number ?? 'N/A' }}
-                                </div>
-                                <div class="firma-box" style="left: 12px;">
-                                    <div class="firma-line"></div>
-                                    <div class="firma-label">FIRMA</div>
-                                </div>
-                            </td>
-                            <td style="width: 4%;"></td>
-                            <td class="signature-cell" style="width: 48%;">
-                                <div class="signature-header">NOMBRE Y DIRECCIÓN DEL AVAL</div>
-                                <div class="signature-body">
-                                    {{ strtoupper($venta->aval->full_name ?? 'N/A') }}<br>
-                                    {{ strtoupper($venta->aval->calle ?? '') }} {{ $venta->aval->numero_exterior ?? '' }}, COL. {{ strtoupper($venta->aval->colonia ?? '') }}<br>
-                                    {{ strtoupper($venta->aval->localidad_domicilio ?? 'LA PAZ, B.C.S.') }}, C.P. {{ $venta->aval->codigo_postal ?? '' }}<br>
-                                    TEL: {{ $venta->aval->phones->first()->number ?? 'N/A' }}
-                                </div>
-                                <div class="firma-box" style="right: 12px;">
-                                    <div class="firma-line"></div>
-                                    <div class="firma-label">FIRMA</div>
-                                </div>
-                            </td>
-                        </tr>
-                    </table>
                 </div>
-            </div>
+            @endforeach
         </div>
-
-        @if (($index + 1) % 4 == 0 && ($index + 1) < count($pagares))
-            <div class="page-break"></div>
-        @endif
     @endforeach
 </body>
 
